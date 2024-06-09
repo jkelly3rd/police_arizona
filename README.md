56a6f6cd9c47ae0e04fdf0e6195ff65087071592
John Kelly <jkelly3rd@gmail.com>
2024-06-08 21:21:01 -0400

    Update readme, data, script

--- README.md
+++ README.md
@@ -1,28 +1,39 @@
 # Arizona Officer History Data Processing
 
-This script processes data obtained from the state of Arizona that includes personnel information and employment history for all officers certified in the state. It performs several operations to clean, standardize, and reformat the data. The original data is preserved in a csv format for reference, and a standardized index is created for further analysis.
+These data were obtained under the state open records law from the [Arizona Peace Officer Standards and Training Board](https://post.az.gov). 
+
+The data released includes personnel and employment history for all officers certified in the state going back to the 1950s. Our processing performs several operations to clean, standardize, and reformat the data into a work history index file that is consistent with other states' data obtained as part of this tracking project. The original data is preserved in CSV format for reference.
 
 ## R Packages Used
 
-- tidyverse: For data manipulation and visualization
-- lubridate: For handling date-time data
-- readxl: For reading Excel files
-- janitor: For cleaning data and managing the workspace
+- `tidyverse`: For data manipulation and visualization
+- `lubridate`: For handling date-time data
+- `readxl`: For reading Excel files
+- `janitor`: For cleaning data and managing the workspace
 
 ## Data Files
 
-The input data is an Excel file named `006_PR_2023_0413_AllOfficers_w_AppointmentsAndFinalActions.xlsx` located in the `data/source/` directory. The file contains employment history data for officers in Arizona.
+The APOST provided one Excel data file in response to a state public records request:
 
-## Data Cleaning
+1. `006_PR_2023_0413_AllOfficers_w_AppointmentsAndFinalActions.xlsx`: Contains certification and work history data for law enforcement officers in Arizona.
+
+## Data Cleaning and Processing
 
 The data cleaning process involves several steps:
 
-- Importing the Excel file and cleaning up the column names for consistency with the index files created for other states in the project.
-- Splitting the full name into first name, middle name, last name, and suffix. Cleaning up the suffixes. Reassembling the full name.
+- Importing the Excel file and cleaning up the column names for consistency with the index files created for other states in the project. 
+- Including splitting the full name into first name, middle name, last name, and suffix. Cleaning up the suffixes. Reassembling the full name.
+- In a few cases, manual corrections were made to the names suffixes after additional reporting and research to confirm the names were correct.
 
 ## Output
 
-The cleaned data is written to two CSV files: `az-2023-index.csv` and `az-2023-original-employment.csv`. The index file contains a simplified version of the data for easy reference, while the original employment file contains the full cleaned data.
+The script generates three CSV files:
+
+1. `az-2023-original-employment.csv`: Contains work history information data in csv format with all original data and fields provided by the state.
+2. `az-2023-index-enhanced.csv`: Contains a standardized index of officers, with additional fields indicating the current status of the officers' license, including if it is revoked or suspended. This certificate status field is a  snapshot in time from when the state released the data.
+3. `az-2023-index.csv`: Contains a standardized index of officers.
+
+The output files are stored in the `data/processed/` directory.
 
 ## Questions or suggestions for improvement?
 
--- scripts/az_certification_processing.R
+++ scripts/az_certification_processing.R
@@ -1,24 +1,26 @@
-# Import required libraries
+# Load required libraries
 library(tidyverse)
 library(lubridate)
 library(readxl)
 library(janitor)
 
-# Clear the workspace to avoid conflicts with existing variables
+# Clear workspace
 rm(list = ls())
 
-# Define the root directory and the output directory
+# Define root directory and output directory
 root_dir = getwd()
 output_dir = "data/processed/"
 
-# Define the paths to the input Excel files
+# Define paths to the source Excel file from state of Arizona
 employment_file = "data/source/006_PR_2023_0413_AllOfficers_w_AppointmentsAndFinalActions.xlsx"
 
-# Define the paths to the output CSV files
+# Define paths to the output CSV files
 az_index = "data/processed/az-2023-index.csv"
+az_index_enhanced = "data/processed/az-2023-index-enhanced.csv"
 az_original_employment = "data/processed/az-2023-original-employment.csv"
 
-# Create a template dataframe for the officers index. This will be used to ensure that the final dataframe has the correct structure.
+# Create a template dataframe for the officers index.
+# This will be used to ensure that the final dataframe has the correct structure.
 template_index <- data.frame("person_nbr" = character(0),
                              "full_name" = character(0),
                              "first_name" = character(0),
@@ -34,53 +36,54 @@ template_index <- data.frame("person_nbr" = character(0),
                              "end_date" = as.Date(character(0))
 )
 
-# Import the Excel files, converting date columns with hard NULL values to NA. Also convert SC's placeholder for null (1/1/1111) to NA.
+# Import the Excel files, converts date columns with hard NULL values to NA.
 # The janitor::clean_names() function is used to clean up the column names.
 az_history <- read_excel(employment_file, 
                          col_types = c("text", "text", "text","date","date","text","text","text",
                                        "date", "text", "text", "text")) %>% janitor::clean_names()
-# name columns for consistency
+# name columns for consistency with template index
 colnames(az_history) <- c("person_nbr","full_name","agency","start_date","end_date","rank",
                           "current_certificate_status","last_action","date_last_action","case_numbers",
                           "final_actions","final_action_dates")
 
-# output the original employment data
+# Output original employment data as CSV
 write_csv(az_history, az_original_employment)
 
-
-# drop columns 8-12 that are not needed for the officer index for AZ
+# Prepare the index consistent with other states' officer work history files
+# Drop columns 8-12 that are not needed for the officer index for AZ
 az_history <- az_history %>% select(-c("last_action","date_last_action","case_numbers","final_actions","final_action_dates"))
 
 # Change the full_name to title case
 az_history$full_name <- str_to_title(az_history$full_name)
-# Get the last name from everything prior to the first comma
+# Extract last name from everything prior to the first comma
 az_history$last_name <- gsub(",.*", "", az_history$full_name)
-# Get the rest_name from everything after the first ", "
+# Assign rest_name from everything after the first ", "
 az_history$rest_name <- gsub(".*, ", "", az_history$full_name)
-
+# Split rest name into tentative component pieces
 split_names <- strsplit(az_history$rest_name, " ")
-
-# Assign the corresponding elements from the split name to new columns
+# Temporarily assign elements from the split name to new columns
 az_history$first_name <- sapply(split_names, '[', 1)
 az_history$middle_name <- sapply(split_names, '[', 2)
 az_history$suffix <- sapply(split_names, '[', 3)
 
-# If suffix is not NA and is not equal to "Jr","II" or "III" then append it to the existing middle_name column and delete it from the suffix column
+# Clean up inconsistent name part fields after manual testing and checks to ID tasks needed
+
+# If suffix is not NA and is not equal to "Jr","II" or "III" 
+# then append to middle_name and delete it from the suffix
 az_history$middle_name <- ifelse(is.na(az_history$suffix), az_history$middle_name, paste(az_history$middle_name, az_history$suffix, sep = " "))
 az_history$suffix <- ifelse(az_history$suffix %in% c("Jr","II","III"), az_history$suffix, NA)
-
 # If there is " Jr" in the last name column, remove it from the last name column and append it to the suffix column
 az_history$suffix <- ifelse(grepl(" Jr", az_history$last_name), "Jr", az_history$suffix)
 az_history$last_name <- gsub(" Jr", "", az_history$last_name)
-# If there is a Jr in the middle name, remove it from the middle name column and append it to the suffix column, ignoring case
+# If there is a Jr in the middle name, remove it from the middle name column 
+# and append it to the suffix column, ignoring case
 az_history$suffix <- ifelse(grepl("Jr", az_history$middle_name, ignore.case = TRUE), "Jr", az_history$suffix)
 az_history$middle_name <- gsub("Jr", "", az_history$middle_name, ignore.case = TRUE)
-# Fix oddly formatted names individually
+# Fix oddly formatted names individually after research to verify
 az_history$first_name[az_history$person_nbr == 15416] <- "Santiago"
 az_history$middle_name[az_history$person_nbr == 15416] <- NA
 az_history$last_name[az_history$person_nbr == 15416] <- "Renteria"
 az_history$suffix[az_history$person_nbr == 15416] <- "Jr"
-
 # If there is " Ii" or " Iii" in the last name column, remove it from the last name column and append it to the suffix column
 az_history$suffix <- ifelse(grepl(" Iii", az_history$last_name), "III", az_history$suffix)
 az_history$last_name <- gsub(" Iii", "", az_history$last_name)
@@ -94,20 +97,17 @@ az_history$full_name <- str_squish(az_history$full_name)
 # Drop rest_name
 az_history <- az_history %>% select(-rest_name)
 
-# Now merge the cleaned South Carolina data into the index template
+# Now merge the cleaned Arizona data into the index template
 az_officers_index <- bind_rows(template_index,az_history)
 
-# Export csv for project
-az_officers_index %>% write_csv(az_index)
+# Export csv of work history index for project
+az_officers_index %>% write_csv(az_index_enhanced)
 
+# Remove extra columns and export csv of standard work history index for project
+az_officers_index <- az_officers_index %>% select(person_nbr, full_name, first_name, middle_name, last_name, suffix, year_of_birth, age, agency, type, rank, start_date, end_date)
 
-
-
-
-
-
-
-
+# Export csv of standard work history index for project
+az_officers_index %>% write_csv(az_index)     
 
 
 
