a6ddbd1003aa59ffeb2b6cd0fdc56d13e9a0e33a
John Kelly <jkelly3rd@gmail.com>
2024-06-26 15:35:32 -0400

    Update AZ code for date, name formatting

--- scripts/az_certification_processing.R
+++ scripts/az_certification_processing.R
@@ -53,8 +53,8 @@ write_csv(az_history, az_original_employment)
 # Drop columns 8-12 that are not needed for the officer index for AZ
 az_history <- az_history %>% select(-c("last_action","date_last_action","case_numbers","final_actions","final_action_dates"))
 
-# Change the full_name to title case
-az_history$full_name <- str_to_title(az_history$full_name)
+# Change the full_name to upper case
+az_history$full_name <- str_to_upper(az_history$full_name)
 # Extract last name from everything prior to the first comma
 az_history$last_name <- gsub(",.*", "", az_history$full_name)
 # Assign rest_name from everything after the first ", "
@@ -66,29 +66,34 @@ az_history$first_name <- sapply(split_names, '[', 1)
 az_history$middle_name <- sapply(split_names, '[', 2)
 az_history$suffix <- sapply(split_names, '[', 3)
 
+# Ensure start_date is yyyymmdd format using ymd
+az_history$start_date <- ymd(az_history$start_date)
+# Ensure end_date is yyyymmdd format using ymd
+az_history$end_date <- ymd(az_history$end_date)
+
 # Clean up inconsistent name part fields after manual testing and checks to ID tasks needed
 
 # If suffix is not NA and is not equal to "Jr","II" or "III" 
 # then append to middle_name and delete it from the suffix
 az_history$middle_name <- ifelse(is.na(az_history$suffix), az_history$middle_name, paste(az_history$middle_name, az_history$suffix, sep = " "))
-az_history$suffix <- ifelse(az_history$suffix %in% c("Jr","II","III"), az_history$suffix, NA)
+az_history$suffix <- ifelse(az_history$suffix %in% c("JR","II","III"), az_history$suffix, NA)
 # If there is " Jr" in the last name column, remove it from the last name column and append it to the suffix column
-az_history$suffix <- ifelse(grepl(" Jr", az_history$last_name), "Jr", az_history$suffix)
+az_history$suffix <- ifelse(grepl(" JR", az_history$last_name), "JR", az_history$suffix)
 az_history$last_name <- gsub(" Jr", "", az_history$last_name)
-# If there is a Jr in the middle name, remove it from the middle name column 
+# If there is a JR in the middle name, remove it from the middle name column 
 # and append it to the suffix column, ignoring case
-az_history$suffix <- ifelse(grepl("Jr", az_history$middle_name, ignore.case = TRUE), "Jr", az_history$suffix)
-az_history$middle_name <- gsub("Jr", "", az_history$middle_name, ignore.case = TRUE)
+az_history$suffix <- ifelse(grepl("JR", az_history$middle_name, ignore.case = TRUE), "JR", az_history$suffix)
+az_history$middle_name <- gsub("JR", "", az_history$middle_name, ignore.case = TRUE)
 # Fix oddly formatted names individually after research to verify
-az_history$first_name[az_history$person_nbr == 15416] <- "Santiago"
+az_history$first_name[az_history$person_nbr == 15416] <- "SANTIAGO"
 az_history$middle_name[az_history$person_nbr == 15416] <- NA
-az_history$last_name[az_history$person_nbr == 15416] <- "Renteria"
-az_history$suffix[az_history$person_nbr == 15416] <- "Jr"
-# If there is " Ii" or " Iii" in the last name column, remove it from the last name column and append it to the suffix column
-az_history$suffix <- ifelse(grepl(" Iii", az_history$last_name), "III", az_history$suffix)
-az_history$last_name <- gsub(" Iii", "", az_history$last_name)
-az_history$suffix <- ifelse(grepl(" Ii", az_history$last_name), "II", az_history$suffix)
-az_history$last_name <- gsub(" Ii", "", az_history$last_name)
+az_history$last_name[az_history$person_nbr == 15416] <- "RENTERIA"
+az_history$suffix[az_history$person_nbr == 15416] <- "JR"
+# If there is " II" or " III" in the last name column, remove it from the last name column and append it to the suffix column
+az_history$suffix <- ifelse(grepl(" III", az_history$last_name), "III", az_history$suffix)
+az_history$last_name <- gsub(" III", "", az_history$last_name)
+az_history$suffix <- ifelse(grepl(" II", az_history$last_name), "II", az_history$suffix)
+az_history$last_name <- gsub(" II", "", az_history$last_name)
 
 # Reassemble the full name from first_name, middle_name, last_name, and suffix
 az_history$full_name <- paste(az_history$first_name, ifelse(is.na(az_history$middle_name),"",az_history$middle_name), az_history$last_name, ifelse(is.na(az_history$suffix),"",az_history$suffix), sep = " ")
